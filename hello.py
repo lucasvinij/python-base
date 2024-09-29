763dd646e2529e6a13ad8e222d6e6d65c47248c7
lucasvinij <lucasvinij@gmail.com>
2024-09-29 13:56:16 -0300

    Adicionado hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,5 +1,33 @@
 #! /usr/bin/env python3
-print("Hello, World!".upper()) #test-ignore
-# comentario 
-# treino git 
-print("FIM PROGRAMA")
+"""Hello World Multi Linguas.
+
+Dependendo da linguam configurada no ambiente 
+o programa exibe a mensagem correspondente.
+
+Usage: 
+Tenha a variavel LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execution:
+    python3 hello.py
+    ./hello.py 
+"""
+
+__version__ = "0.0.1" #Dunder
+__author__ = "Lucas"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US").split(".")[0]
+
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+        
+print(msg)
+
